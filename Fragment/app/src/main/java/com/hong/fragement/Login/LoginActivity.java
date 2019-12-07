bcb59c0bb094a7448197466af60f78d3a33af27c
Seong <ohsg0315@naver.com>
2019-12-07 22:07:14 +0900

    페이스북 로그인 연동 by 오성

--- Fragment/app/src/main/java/com/hong/fragement/Login/LoginActivity.java
+++ Fragment/app/src/main/java/com/hong/fragement/Login/LoginActivity.java
@@ -14,6 +14,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -32,6 +39,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
@@ -64,12 +72,36 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button signUpBtn;
     private Button idFind;
     private SignInButton googleBtn; // 구글 로그인 버튼
+    public CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginButton facebookBtn = findViewById(R.id.login_button);
+        facebookBtn.setReadPermissions("email", "public_profile");
+        facebookBtn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+            }
+        });
+
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -83,6 +115,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         auth = FirebaseAuth.getInstance();  // 파이어베이스 인증 객체 초기화
 
+        initSettings();
+    }
+
+    private void initSettings() {
         googleBtn = findViewById(R.id.google_btn);
         userId = findViewById(R.id.user_id);
         userPassowrd = findViewById(R.id.user_password);
@@ -120,7 +156,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             startIdFind();
         else if (view == logo)
             myStartActivity(MainActivity.class);
-
     }
 
     // 구글 로그인
@@ -171,6 +206,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     // 인증 성공시 Intent
     public void updateUI(FirebaseUser user) {
         if (user != null) {
+            Log.d(TAG, "가자가자");
             Intent intent = new Intent(this, MainActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
             startActivity(intent);
@@ -186,6 +222,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
@@ -237,6 +274,30 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             //  Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
                             updateUI(null);
                         }
+                        // ...
+                    }
+                });
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = auth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            updateUI(null);
+                        }
+
                         // ...
                     }
                 });
