5b86ac68579aa8dd6b32c6e0f2cbabc5e5f707d1
hong <wnhong96@naver.com>
2019-12-20 18:31:31 +0900

    보완

--- Fragment/app/src/main/java/com/hong/fragement/Home/HomeFragment.java
+++ Fragment/app/src/main/java/com/hong/fragement/Home/HomeFragment.java
@@ -31,6 +31,7 @@ import com.hong.fragement.MovieObj;
 import com.hong.fragement.NewMoviePage.NewMoviePage;
 import com.hong.fragement.R;
 import com.hong.fragement.SearchResult.SearchResultPage;
+import com.hong.fragement.Top100Page.AdapterForTop100;
 import com.hong.fragement.Top100Page.Top100Page;
 
 import java.util.ArrayList;
@@ -50,8 +51,10 @@ public class HomeFragment extends Fragment {
 
     private MovieObj data;
     private HomeAdapter adapter;
+    private Top100Adapter adapter2;
     private ArrayList<String> titleList;
     private ArrayList<MovieObj> datList;
+    private ArrayList<MovieObj> datList2;
 
     @Nullable
     @Override
@@ -76,7 +79,8 @@ public class HomeFragment extends Fragment {
         recyclerView2.setHasFixedSize(true); // 리사이클러뷰 안의 아이템 크기를 일정하게 고정
         recyclerView2.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL,false));
 
-        readMovieData(); // DB에서 데이터를 빼와 리스트에 담고 adapter를 생성, 세팅하는 메서드
+        readMovieData();    // 최신영화 : DB에서 데이터를 빼와 리스트에 담고 adapter를 생성, 세팅하는 메서드
+        readMovieData2();   // Top100영화
 
         // 서치바에 띄어줄 데이터를 담을 리스트를 생성하고 연결
         titleList = new ArrayList<String>();
@@ -100,9 +104,38 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
-    // DB에서 데이터를 가져와 리사이클러뷰에 세팅해주는 메서드
+    // 최신영화 : DB에서 데이터를 가져와 리사이클러뷰에 세팅해주는 메서드
     private void readMovieData() {
         datList = new ArrayList<MovieObj>();
+
+        db.collection("Movie").limit(10).get()    // DB의 "Movie" 컬렉션에서 데이터 가져옴
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {   // 스냅샷을 떠서 MovieObj 타입으로 변환 후 리스트에 담음
+                                data = new MovieObj();
+                                data.setTitle(document.toObject(MovieObj.class).getTitle());
+                                data.setImageUri(document.toObject(MovieObj.class).getImageUri());
+                                data.setPrice(document.toObject(MovieObj.class).getPrice());
+                                data.setSummary(document.toObject(MovieObj.class).getSummary());
+                                data.setYoutubeUri(document.toObject(MovieObj.class).getYoutubeUri());
+                                data.setGenre(document.toObject(MovieObj.class).getGenre());
+
+                                titleList.add(document.toObject(MovieObj.class).getTitle());
+                                datList.add(data);
+                            }
+                            Toast.makeText(getActivity(),"아아아",Toast.LENGTH_SHORT).show();
+                            adapter = new HomeAdapter(datList, getActivity(),listener); // 데이터 생성. Context : getActivity (프래그먼트 이용)
+                            recyclerView1.setAdapter(adapter);
+                        }
+                    }
+                });
+    }
+
+    // 최신영화 : DB에서 데이터를 가져와 리사이클러뷰에 세팅해주는 메서드
+    private void readMovieData2() {
+        datList2 = new ArrayList<MovieObj>();
 
         db.collection("Movie").get()    // DB의 "Movie" 컬렉션에서 데이터 가져옴
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -116,15 +149,20 @@ public class HomeFragment extends Fragment {
                                 data.setPrice(document.toObject(MovieObj.class).getPrice());
                                 data.setSummary(document.toObject(MovieObj.class).getSummary());
                                 data.setYoutubeUri(document.toObject(MovieObj.class).getYoutubeUri());
+                                data.setGenre(document.toObject(MovieObj.class).getGenre());
 
                                 titleList.add(document.toObject(MovieObj.class).getTitle());
-                                datList.add(data);
+                                datList2.add(data);
                             }
-                            adapter = new HomeAdapter(datList, getActivity(),listener); // 데이터 생성. Context : getActivity (프래그먼트 이용)
-                            recyclerView1.setAdapter(adapter);
+                            adapter2 = new Top100Adapter(datList2, getActivity(),listener); // 데이터 생성. Context : getActivity (프래그먼트 이용)
+                            recyclerView2.setAdapter(adapter2);
                         }
                     }
                 });
+    }
+
+    public interface OnItemClick {
+        void onMovieSelected(MovieObj selectedMovie);
     }
 
     // 영화 포스터 클릭시 상세페이지 전환 이벤트
@@ -137,14 +175,19 @@ public class HomeFragment extends Fragment {
             Map<String, Integer> moviePrice = selectedMovie.getPrice();
             String naverPrice = moviePrice.get("네이버").toString();
             String wavePrice = moviePrice.get("웨이브").toString();
+            String yesPrice = moviePrice.get("예스24").toString();
+            String playPrice = moviePrice.get("플레이스토어").toString();
 
             intent.putExtra("imageUri",selectedMovie.getImageUri());
             intent.putExtra("title",selectedMovie.getTitle());
             intent.putExtra("price1",naverPrice);
             intent.putExtra("price2",wavePrice);
+            intent.putExtra("price3",yesPrice);
+            intent.putExtra("price4",playPrice);
             intent.putExtra("summary",selectedMovie.getSummary());
             intent.putExtra("title",selectedMovie.getTitle());
             intent.putExtra("youtubeUri",selectedMovie.getYoutubeUri());
+            intent.putExtra("genre",selectedMovie.getGenre());
             intent.putExtra("dataFlag","2");
 
             startActivity(intent);
@@ -168,10 +211,6 @@ public class HomeFragment extends Fragment {
             }
         }
     };
-
-    public interface OnItemClick {
-        void onMovieSelected(MovieObj selectedMovie);
-    }
 
     private View.OnClickListener btnPageIntentListener = new View.OnClickListener() {
         @Override
